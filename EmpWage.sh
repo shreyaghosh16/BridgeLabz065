30e13223e55f71645b6a29276f9b2a00d5631a08
shreyaghosh16 <shreyaghosh95@gmail.com>
2020-08-05 12:25:41 +0530

    Adding isPresent to check if Employee is present or not

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,3 +2,10 @@
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
+isPresent=$((RANDOM%2))
+if [ $isPresent == 0 ]
+then
+   echo "The Employee is absent"
+else 
+   echo "The Employee is present"
+fi
